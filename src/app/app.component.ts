6ad173cf1c0d812af746bf27491e6600048646ac
Tanelo <tanello.mondaca@gmail.com>
2022-08-09 12:19:45 -0400

    primeNg module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+
+  nombre: string = 'TaNeLo MoNdAca';
+
+  mostrarConsola(){
+    console.log(this.nombre);
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// Modulo personalizado de PrimeNg
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PrimeNgModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
